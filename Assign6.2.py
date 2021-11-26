08f38be637c921873d2b15f75325adb4566bd453
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-26 12:25:30 +0800

    ADDITION TRAINER

--- Assign6.2.py
+++ Assign6.2.py
@@ -1,4 +1,9 @@
+print("         ADDITION TRAINER")
 
+name = input("Name: ")
+nickname = input ("Nickname: ")
+
+print(f"    Thank You {nickname} for using Addition Trainer. Have fun and learn the Addition!")
 
 import random
 
@@ -8,14 +13,20 @@ for x in range(10):
     firstnum = random.randint(0,99)
     secondnum = random.randint(0,99)
     print(f"Add {firstnum} + {secondnum}")
-    answer = int(input("Enter your answer: "))
+    answer = int(input("    Enter your answer: "))
     answerf = firstnum + secondnum
 
     if answer == answerf:
-        print("CORRECT")
+        print("    CORRECT")
         score = score + 1
     else :
-        print(f"INCORRECT, The answer is {answerf}")
+        print(f"    INCORRECT, The answer is {answerf}")
 
-print(f"Your score is {score}/10.")
+print(f"You obtained a score of {score}/10.")
 
+if score > 6:
+    print("    Job well done. Keep it up.")
+else:
+    print("    Better luck next time.")
+
+print(f"Hopefully you learned something {name}. Thank you for using Addition Trainer.")
