683bc91cbaf46d6be5a68ee56abe4b45aab07747
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-26 11:01:44 +0800

    Number sorter, if third number is the highest

--- Assign6.1.py
+++ Assign6.1.py
@@ -11,6 +11,7 @@ def inputnumber():
 
 num1, num2, num3, num4 = inputnumber()
 
+#if first number is highest
 if num1 > num2 and num2 > num3 and num3 > num4:
     print(num1,num2,num3,num4)
 elif num1> num2 and num2 > num4 and num4 > num3:
@@ -37,3 +38,17 @@ elif num2>num1 and num1>num3 and num3>num4:
     print(num2,num1,num3,num4)
 elif num2>num1 and num1>num4 and num4>num3:
     print(num2,num1,num4,num3)
+
+#if third number is highest
+if num3>num4>num1>num2:
+    print(num3,num4,num1,num2)
+elif num3>num4>num2>num1:
+    print(num3,num4,num2,num1)
+elif num3>num1>num2>num4:
+    print(num3,num1,num2,num4)
+elif num3>num1>num4>num2:
+    print(num3,num1,num4,num2)
+elif num3>num2>num1>num4:
+    print(num3,num2,num1,num4)
+elif num3>num2>num4>num1:
+    print(num3,num2,num4,num1)
