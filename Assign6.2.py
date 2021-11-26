72fb6e659a248af3892c2e17b3f6594980a01a64
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-26 11:50:47 +0800

    have an answer and kkow the score

--- Assign6.2.py
+++ Assign6.2.py
@@ -1,5 +1,21 @@
+
+
 import random
+
+score = 0
+
 for x in range(10):
     firstnum = random.randint(0,99)
     secondnum = random.randint(0,99)
-    print(f"What is {firstnum} + {secondnum}?")
+    print(f"Add {firstnum} + {secondnum}")
+    answer = int(input("Enter your answer: "))
+    answerf = firstnum + secondnum
+
+    if answer == answerf:
+        print("CORRECT")
+        score = score + 1
+    else :
+        print(f"INCORRECT, The answer is {answerf}")
+
+print(f"Your score is {score}/10.")
+
