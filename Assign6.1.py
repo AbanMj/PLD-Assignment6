967efc36ab3fca1e454d469676a89725ece2a014
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-26 11:22:27 +0800

    NUMBER SORTER from highest to lowest

--- Assign6.1.py
+++ Assign6.1.py
@@ -3,40 +3,40 @@
 #Print the 4 numbers from highest to lowest using only if-else statement.
 
 def inputnumber():
-    first = float(input("   Enter 1st number: "))
-    second = float(input("   Enter 2nd number: "))
-    third = float(input("   Enter 3rd number: "))
-    fourth = float(input("   Enter 4th number: "))
+    first = int(input("   Enter 1st number: "))
+    second = int(input("   Enter 2nd number: "))
+    third = int(input("   Enter 3rd number: "))
+    fourth = int(input("   Enter 4th number: "))
     return first, second, third, fourth
 
 num1, num2, num3, num4 = inputnumber()
 
 #if first number is highest
-if num1 > num2 and num2 > num3 and num3 > num4:
+if num1>num2>num3>num4:
     print(num1,num2,num3,num4)
-elif num1> num2 and num2 > num4 and num4 > num3:
+elif num1>num2>num4>num3:
     print(num1,num2,num4,num3)
-elif num1> num3 and num3>num2 and num2>num4:
+elif num1>num3>num2>num4:
     print(num1,num3,num2,num4)
-elif num1> num3 and num3>num4 and num4>num2:
+elif num1>num3>num4>num2:
     print(num1,num3,num4,num2)
-elif num1 >num4 and num4>num2 and num2>num3:
+elif num1>num4>num2>num3:
     print(num1,num4,num2,num3)
-elif num1>num4 and num4>num3 and num3>2:
+elif num1>num4>num3>2:
     print(num1,num4,num3,num2)
 
 #if second number is highest
-if num2>num3 and num3>num4 and num4>num1:
+if num2>num3>num4>num1:
     print(num2,num3,num4,num1)
-elif num2>num3 and num3>num1 and num1>num4:
+elif num2>num3>num1>num4:
     print(num2,num3,num1,num4)
-elif num2>num4 and num4>num1 and num1>num3:
+elif num2>num4>num1>num3:
     print(num2,num4,num1,num3)
-elif num2>num4 and num4>num3 and num3>num1:
+elif num2>num4>num3>num1:
     print(num2,num4,num3,num1)
-elif num2>num1 and num1>num3 and num3>num4:
+elif num2>num1>num3>num4:
     print(num2,num1,num3,num4)
-elif num2>num1 and num1>num4 and num4>num3:
+elif num2>num1>num4>num3:
     print(num2,num1,num4,num3)
 
 #if third number is highest
@@ -66,4 +66,3 @@ elif num4>num3>num1>num2:
     print(num4,num3,num1,num2)
 elif num4>num3>num2>num1:
     print(num4,num3,num2,num1)
-    
