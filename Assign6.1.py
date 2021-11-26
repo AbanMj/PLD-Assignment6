4aa3fdc499281db975061924c22bdbb27cf3cbd7
Mary Joy Aban <abanmaryjoy0@gmail.com>
2021-11-26 10:40:32 +0800

    number sorted if secone number is the highest

--- Assign6.1.py
+++ Assign6.1.py
@@ -23,3 +23,17 @@ elif num1 >num4 and num4>num2 and num2>num3:
     print(num1,num4,num2,num3)
 elif num1>num4 and num4>num3 and num3>2:
     print(num1,num4,num3,num2)
+
+#if second number is highest
+if num2>num3 and num3>num4 and num4>num1:
+    print(num2,num3,num4,num1)
+elif num2>num3 and num3>num1 and num1>num4:
+    print(num2,num3,num1,num4)
+elif num2>num4 and num4>num1 and num1>num3:
+    print(num2,num4,num1,num3)
+elif num2>num4 and num4>num3 and num3>num1:
+    print(num2,num4,num3,num1)
+elif num2>num1 and num1>num3 and num3>num4:
+    print(num2,num1,num3,num4)
+elif num2>num1 and num1>num4 and num4>num3:
+    print(num2,num1,num4,num3)
